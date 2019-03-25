e39e8f7f6d9a54c6e338ea39f28fedfb57abc814
Oleh Matkovskyi <omatkovskyi@nedelta.com>
2019-03-26 00:54:22 +0200

    generics new

--- src/generics.js
+++ src/generics.js
@@ -4,3 +4,27 @@ function printSmth(args) {
 }
 printSmth(["One", "Two"]);
 printSmth([1, 2]);
+// Generic types
+function myFunc1(data) {
+    return data;
+}
+function myFuncGen(data) {
+    return data;
+}
+var gen = myFunc1;
+var gen1 = myFuncGen;
+console.log(gen("lol"));
+console.log(gen1(555));
+// Generic class
+var SimpleMAth = /** @class */ (function () {
+    function SimpleMAth() {
+    }
+    SimpleMAth.prototype.calculate = function () {
+        return +this.FirstValue * +this.SecondValue;
+    };
+    return SimpleMAth;
+}());
+var mathClass = new SimpleMAth();
+mathClass.FirstValue = "10";
+mathClass.SecondValue = 20;
+console.log(mathClass.calculate());
--- src/generics.ts
+++ src/generics.ts
@@ -7,4 +7,34 @@ function printSmth<T>(args: T[]) {
 printSmth(["One", "Two"]);
 
 printSmth<number>([1, 2]);
+
+// Generic types
+
+function myFunc1(data: any) {
+    return data;
+}
+
+function myFuncGen<T>(data: T) {
+    return data;
+}
+
+const gen: <T>(data: T) => T = myFunc1;
+const gen1: <T>(data: T) => T = myFuncGen;
+console.log( gen("lol"));
+console.log( gen1<number>(555));
     
+// Generic class
+
+class SimpleMAth<T extends number | string> {
+    FirstValue: T;
+    SecondValue: T;
+    calculate() {
+        return +this.FirstValue * +this.SecondValue;
+    }    
+}
+
+const mathClass = new SimpleMAth();
+mathClass.FirstValue = "10";
+mathClass.SecondValue = 20;
+
+console.log(mathClass.calculate());
