c50a1152e2e4b24910afc82ba85abbfd8c7ddb63
Oleh Matkovskyi <omatkovskyi@nedelta.com>
2019-03-28 16:46:24 +0200

    Decorators final

--- src/decorators.js
+++ src/decorators.js
@@ -4,6 +4,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 console.log("*** Decorators ***");
 function logger(constructorFn) {
     console.log(constructorFn);
@@ -26,7 +29,86 @@ var secondClass = /** @class */ (function () {
         console.log("Hi second constructor");
     }
     secondClass = __decorate([
-        logOrNot(false)
+        logOrNot(true)
     ], secondClass);
     return secondClass;
 }());
+// advanced
+function printMethod(constructorFn) {
+    constructorFn.prototype.print = function () {
+        console.log(this);
+    };
+}
+var Plant = /** @class */ (function () {
+    function Plant() {
+        this.name = "Green tea";
+    }
+    Plant = __decorate([
+        logOrNot(true),
+        printMethod
+    ], Plant);
+    return Plant;
+}());
+var a = new Plant();
+a.print();
+// Method Decorators 
+function editable(value) {
+    return function (target, propName, descriptor) {
+        descriptor.writable = value;
+    };
+}
+function overWritable(value) {
+    return function (target, propName) {
+        var newDescriptor = {
+            writable: value
+        };
+        return newDescriptor;
+    };
+}
+function prontParamInfo(target, methodName, paramIndex) {
+    console.log("Target is: " + target);
+    console.log("Method name is: " + methodName);
+    console.log("Index is: " + paramIndex);
+}
+var Project = /** @class */ (function () {
+    function Project(name) {
+        this.projName = name;
+    }
+    Project.prototype.calcBudget = function () {
+        console.log(3000);
+    };
+    __decorate([
+        overWritable(false)
+    ], Project.prototype, "projName", void 0);
+    __decorate([
+        editable(false)
+    ], Project.prototype, "calcBudget", null);
+    return Project;
+}());
+var pr = new Project("Test project");
+console.log(pr);
+pr.calcBudget();
+pr.calcBudget = function () {
+    console.log(5000);
+};
+pr.calcBudget();
+var Course = /** @class */ (function () {
+    function Course(name) {
+        this.name = name;
+    }
+    Course.prototype.printStudentNumbers = function (mode, printAll) {
+        if (printAll) {
+            console.log(100);
+        }
+        else {
+            console.log(10);
+        }
+    };
+    __decorate([
+        __param(1, prontParamInfo)
+    ], Course.prototype, "printStudentNumbers", null);
+    return Course;
+}());
+var course = new Course("Some course");
+course.printStudentNumbers("smth", true);
+course.printStudentNumbers("smth", false);
--- src/decorators.ts
+++ src/decorators.ts
@@ -12,16 +12,98 @@ class MyClass {
 }
 
 // Factory
-
 function logOrNot(value: boolean) {
     
     return  value ? logger : null;
 }
 
-@logOrNot(false)
+@logOrNot(true ) 
 class secondClass {
     constructor()
     {
         console.log("Hi second constructor");
     }
 }
+
+// advanced
+function printMethod(constructorFn: Function) {
+    constructorFn.prototype.print = function() {
+        console.log(this);
+    }
+}
+
+@logOrNot(true)
+@printMethod
+class Plant {
+    name: string = "Green tea";
+}
+
+let a = new Plant();
+(<any>a).print();
+
+// Method Decorators 
+function editable(value: boolean) {
+    return function(target: any, propName: string, descriptor: PropertyDescriptor) {
+        descriptor.writable = value;
+    }
+}
+
+function overWritable(value: boolean) {
+    return function(target: any, propName: string): any {        
+        const newDescriptor: PropertyDescriptor = {
+            writable: value
+        };
+        return newDescriptor;            
+    }
+}
+
+function prontParamInfo(target: any, methodName: string, paramIndex: number) {
+    console.log(`Target is: ${target}`);
+    console.log(`Method name is: ${methodName}`);
+    console.log(`Index is: ${paramIndex}`);
+}
+
+class Project {
+    @overWritable(false)
+    projName: string;
+
+    constructor(name: string) {
+        this.projName = name;
+    }
+
+    @editable(false)
+    calcBudget() {
+        console.log(3000);
+    }
+}
+
+const pr = new Project("Test project");
+console.log(pr);
+pr.calcBudget();
+
+pr.calcBudget = function() {
+    console.log(5000);
+}
+
+pr.calcBudget(); 
+
+
+class Course {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    printStudentNumbers(mode: string, @prontParamInfo printAll: boolean) {
+        if (printAll) {
+            console.log(100);
+        } else {
+            console.log(10);
+        }
+    }
+}
+
+let course = new Course("Some course");
+course.printStudentNumbers("smth", true);
+course.printStudentNumbers("smth", false);
